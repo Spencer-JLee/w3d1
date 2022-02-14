641b018d9b1a84c83ea8f8540f18867f2ab4b028
Spencer-JLee <spencer.lee.careers@gmail.com>
2022-02-14 17:25:41 -0500

    added txt file

--- game.rb
+++ game.rb
@@ -1,13 +1,24 @@
+require_relative 'player.rb'
+
 class Game
 
-    def initialize(player_number)
-        @players = Array.new(player_number)
+    def initialize(player_names)
+        @players = Array.new(player_names.length) {Array.new(2, 0)}
         @fragment = ""
         @dictionary = {}
+        File.open("./dictionary.txt") do |n|
+            n.each do |line|
+                key = line.chomp
+                @dictionary[key] = nil
+            end
+        end
     end
 
     def play_round
-
+        until !take_turn(@current_player)
+            next_player!
+        end
+        puts "#{@current_player.name} has lost."
     end
 
     def current_player
@@ -24,6 +35,7 @@ class Game
 
     def take_turn(player)
         str = ""
+        puts "The current fragment is #{@fragment}."
         until valid_play?(str)
             puts "Please enter a letter"
             str = gets.chomp
@@ -41,5 +53,4 @@ class Game
             false
         end
     end
-    
 end
