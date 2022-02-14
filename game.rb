693703d4c8cb940355aba0c9ee9e129a3ddef881
Spencer-JLee <spencer.lee.careers@gmail.com>
2022-02-14 16:35:51 -0500

    ghost

---
+++ game.rb
@@ -0,0 +1,31 @@
+class Game
+    def initialize(player_number)
+        @players = Array.new(player_number)
+        @fragment = ""
+        @dictionary = {}
+    end
+
+    def play_round
+
+    end
+
+    def current_player
+
+    end
+
+    def previous_player
+
+    end
+
+    def next_player
+
+    end
+
+    def take_turn(player)
+
+    end
+
+    def valid_play?(str)
+        
+    end
+end
