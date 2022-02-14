f40cb4a0afb69a8eef705aa92309e11d93edb5d5
Nicholas Angleton <nick.angleton@gmail.com>
2022-02-14 16:22:01 -0500

    commit

--- enumerables.rb
+++ enumerables.rb
@@ -35,15 +35,20 @@ class Array
 
     def my_flatten
         arr = []
-        return [self] if !self.is_a? Array
-        self.my_each {|ele| arr.push(*self.my_flatten)}
+        self.my_each do |ele|
+            if ele.is_a? Array
+                arr.push(*ele.my_flatten)
+            else
+                arr.push(ele)
+            end
+        end
         arr
     end
 
     def my_zip(*arrs)
         arr = Array.new(self.length) { Array.new(arrs.length + 1, nil)}
 
-        self.my_each {|ele| arr[self.index(ele)][0] = ele}
+        self.my_each { |ele| arr[self.index(ele)][0] = ele}
         (1...arrs.length + 1).each do |i|
             arrs[i].my_each {|ele| arr[arrs[i].index(ele)][i] = ele}
         end
@@ -51,5 +56,24 @@ class Array
         arr
     end
 
-    
+    def my_rotate(n = 1)
+        arr = Array.new(self.length)
+        (0...self.length).each { |i| arr[i] = self[(i + n) % self.length] }
+        arr
+    end
+
+    def my_join(separator = "")
+        str = self[0]
+        (1...self.length).each { |i| str += separtor + self[i] }
+        str
+    end
+
+    def my_reverse
+        arr = []
+        (0...self.length).each { |i| arr << self[-1 - i] }
+        arr
+    end
+
 end
+
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
