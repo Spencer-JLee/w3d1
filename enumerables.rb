348fe54f23f270c182b1b68e9b0a898d4d543c50
Nicholas Angleton <nick.angleton@gmail.com>
2022-02-14 15:24:16 -0500

    again

--- enumerables.rb
+++ enumerables.rb
@@ -13,7 +13,29 @@ class Array
 
     def my_select(&prc)
         arr = []
-        self.my_each() {|ele| arr << ele if prc.call(ele)}
-        return arr
+        self.my_each() {|ele| arr << ele if prc.call(ele) }
+        arr
     end
+
+    def my_reject(&prc)
+        arr = []
+        self.my_each() {|ele| arr << ele if !prc.call(ele) }
+        arr
+    end
+
+    def my_any?(&prc)
+        self.my_each { |ele| return true if prc.call(ele) }
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each { |ele| return false if !prc.call(ele) }
+        true
+    end
+
+    def my_flatten
+        return [self] if self.is_a? Array
+        
+    end
+
 end
